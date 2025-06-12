9ebfdf40416132d52bb919451b057e165d1c8b2c
fastestsunil <sunil141292@gmail.com>
2025-06-12 20:46:50 +0530

    Update sidebar menu link for 'Online store' to point to the official website; enhance link handling in Sidebar component for improved navigation and security

--- src/layout/sidebar.tsx
+++ src/layout/sidebar.tsx
@@ -110,8 +110,9 @@ export default function Sidebar({ sideMenu, setSideMenu }: IProps) {
                     )}
                     {menu.title === 'Online store' && (
                       <a
-                        href="https://ewo-frontend.vercel.app/"
+                        href={menu.link}
                         target="_blank"
+                        rel="noopener noreferrer"
                         className={`group cursor-pointer rounded-md relative text-black text-lg font-medium inline-flex items-center w-full transition-colors ease-in-out duration-300 px-5 py-[9px] mb-2 hover:bg-gray sidebar-link-active`}
                       >
                         <span className="inline-block mr-[10px] text-xl">
